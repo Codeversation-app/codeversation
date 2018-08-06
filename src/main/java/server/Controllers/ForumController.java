6735f3e45e2ab031196e4277cf36eab0020ee964
Tara Johnson <tara@madface.com>
2018-08-06 15:41:03 -0700

    Project setup. Server connection working.

---
+++ src/main/java/server/Controllers/ForumController.java
@@ -0,0 +1,12 @@
+package server.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForumController {
+    @RequestMapping("/forum")
+    public String forum() {
+        return "forum";
+    }
+}
