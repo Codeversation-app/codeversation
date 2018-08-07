688f6645daf994ed1871bfff23afc7632a3c093c
Tara Johnson <tara@madface.com>
2018-08-07 00:48:14 -0700

    Hashed passwords capturing in db. Username displays in templates (need to add auth/unauth logic).

--- src/main/java/server/Controllers/AuthController.java
+++ src/main/java/server/Controllers/AuthController.java
@@ -1,14 +1,16 @@
 package server.Controllers;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import server.models.User;
 import server.repositories.UserRepository;
 
 @Controller
-@SessionAttributes("username")
 @RequestMapping("/login")
+@SessionAttributes("username")
 public class AuthController {
     @Autowired
     UserRepository userRepository;
@@ -20,10 +22,17 @@ public class AuthController {
 
     @PostMapping
     public String createUser(
+            Model model,
             @RequestParam String username,
             @RequestParam String password
     ) {
-        User user = userRepository.save(new User(username, password));
+        String hashed = BCrypt.hashpw(password, BCrypt.gensalt(12));
+        User user = userRepository.save(new User(username, hashed));
+        System.out.println("username: " + username);
+        System.out.println("hashed password: " + hashed);
+
+        model.addAttribute("username", username);
+
         return "redirect:/forum";
     }
 
--- src/main/java/server/Controllers/ForumController.java
+++ src/main/java/server/Controllers/ForumController.java
@@ -1,9 +1,12 @@
 package server.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("username")
 public class ForumController {
     @RequestMapping("/forum")
     public String forum() {
