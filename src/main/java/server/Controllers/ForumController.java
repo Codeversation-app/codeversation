e0faf8358030f2b875ca3c2f1af88b3f24bfafc9
user <user@Gurren.gachi.club>
2018-08-07 17:16:57 -0700

    more auth adjustments

--- src/main/java/server/Controllers/ForumController.java
+++ src/main/java/server/Controllers/ForumController.java
@@ -2,19 +2,22 @@ package server.Controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @SessionAttributes("username")
 public class ForumController {
     // Returns the forum template
     @RequestMapping("/forum")
-    public String forum(Model model) {
+    public String forum(Model model, HttpServletRequest request) {
+        if(request.getSession().getAttribute("loggedin").equals(false) ||request.getSession().getAttribute("loggedin").equals((null))){
+            return "redirect:/login";
+        }
         //These both need warnings so the user logs back in.
-        if(!model.containsAttribute("username")){return "redirect:/login";}
-        if(model.asMap().get("username")==null){return "redirect:/login";}
-        return "forum";
+         return "forum";
     }
 }
