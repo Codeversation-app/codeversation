a0fe61b7fc3f6ba1c0722d7d3817400bad7bfdec
brandon-fenty <brandonfenty@gmail.com>
2018-08-10 10:23:10 -0700

    adds logged in status to header, fixes collab route bug

--- src/main/java/server/Controllers/ForumSectionController.java
+++ src/main/java/server/Controllers/ForumSectionController.java
@@ -80,7 +80,7 @@ public class ForumSectionController {
         return "section";
     }
 
-    @RequestMapping("/collabgi")
+    @RequestMapping("/collab")
     public String getCollab (Model model) {
         List<PostThread> postThread = postThreadRepository.findByCategory("collab");
         Iterator<PostThread> iterator = postThread.iterator();
